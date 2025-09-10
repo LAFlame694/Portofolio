2936a07af6acf6992efdf061a22247834cb797a6
LAFlame594 <lilflame694@gmail.com>
2025-09-10 18:24:14 +0300

    worked on responsiveness

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* === typing animation === */
+var typed = new Typed(".typing", {
+    strings: ["", "Web Developer", "Django Developer", "Python Developer"],
+    typeSpeed: 100,
+    BackSpedd: 60,
+    loop: true
+})
